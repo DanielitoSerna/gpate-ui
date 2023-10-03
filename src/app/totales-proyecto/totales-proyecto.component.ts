3d072174eda5a99ae2aa42f9d4f8f77d245d65c2
Esteban Merchan <emerchan@conexia.com>
2023-10-02 19:26:12 -0500

    Se agregan estados a totales por proyecto

--- src/app/totales-proyecto/totales-proyecto.component.ts
+++ src/app/totales-proyecto/totales-proyecto.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppService } from '../app.service';
+import { Paginator } from 'primeng/paginator';
 
 @Component({
   selector: 'app-totales-proyecto',
@@ -8,15 +9,24 @@ import { AppService } from '../app.service';
 })
 export class TotalesProyecto implements OnInit {
 
+  @ViewChild('paginator', {static: false}) paginator: Paginator;
+
   totalElements = 0;
   contracts:[] = [];
-  totales: any = {};
+  totales: any[] = [];
   filter: any = {
     page: 0,
-    size: 15,
+    size: 10,
     orderBy: "proyecto",
     orderAscOrDesc: "asc"
   };
+
+  estados = [
+    {name: 'Activo', code: 'Activo'},
+    {name: 'Inactivo', code: 'Inactivo'} 
+  ];
+
+  search = false;
 
   constructor(public service: AppService) {
     this.loadData();
@@ -53,7 +63,7 @@ export class TotalesProyecto implements OnInit {
     this.service.filter({}, "viewContratoTotal?").then((data: any) => {
       this.service.finishProgress();
       let embedded = data._embedded;
-      this.totales = embedded.viewContratoTotal[0];
+      this.totales = embedded.viewContratoTotal;
     });
   }
 
@@ -61,5 +71,26 @@ export class TotalesProyecto implements OnInit {
     let total = proyecto.pagosAplicados ? proyecto.pagosAplicados * 100 : 0;
     total = proyecto.importeContratado ? total / proyecto.importeContratado: 0;
     return Math.round(total);
+  }
+
+  filterData() {
+    this.search = true;
+    this.paginator.changePage(0);
+    this.filter.page = 0;
+    this.loadData();
+  }
+
+  clean() {
+    if(this.search) {
+      this.search = false;
+      let page = this.filter.page;
+      this.filter = {
+        page: page,
+        size: 10,
+        orderBy: "proyecto",
+        orderAscOrDesc: "asc"
+      }
+      this.loadData();
+    }
   }
 }
